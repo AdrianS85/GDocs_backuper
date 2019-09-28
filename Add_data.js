f835434ae7c459077467a3fe909f9220025cf059
AdrianS85 <adrianstankiewicz85@gmail.com>
2019-09-28 15:49:27 +0200

    Update Add_data.js

--- Add_data.js
+++ Add_data.js
@@ -1,5 +1,5 @@
 /*
-Script for Google Sheet. Creates a Date Stamp if a column is edited. Removes Date Stamp, if cell value is removed
+Creates a Date Stamp if a column is edited. Removes Date Stamp, if cell value is removed
 Based on:
 https://yagisanatode.com/2018/02/21/add-the-current-date-to-a-sheet-when-data-is-added-so-that-the-date-does-not-changestatic-google-sheets/
 */
@@ -10,7 +10,13 @@ https://yagisanatode.com/2018/02/21/add-the-current-date-to-a-sheet-when-data-is
 var COLUMNTOCHECK = 1;
 // Where you want the date time stamp offset from the input location. [row, column]
 var DATETIMELOCATION = [0,2];
- 
+
+var exclude_these_sheets_from_dataStamping = ['instructions', 'sugg']
+/* This does not work as needed, because it ignores operation on entire ranges. 
+Bellow I suggest function that can do something about it:
+e.range - https://developers.google.com/apps-script/guides/triggers/events
+getActiveRangeList() - https://developers.google.com/apps-script/reference/spreadsheet/sheet.html#getActiveRangeList()
+*/
 function onEdit(e) 
 {
   var ss = SpreadsheetApp.getActiveSpreadsheet();
@@ -18,16 +24,21 @@ function onEdit(e)
   //checks that we're on the correct sheet.
   var selectedCell = ss.getActiveCell(); //this is Range class
   //checks the column to ensure it is on the one we want to cause the date to appear.
-  if( selectedCell.getColumn() == COLUMNTOCHECK) 
-  { 
-    var dateTimeCell = selectedCell.offset(DATETIMELOCATION[0],DATETIMELOCATION[1]);
-    if( selectedCell.getDisplayValue() == '')
+  if( exclude_these_sheets_from_dataStamping.indexOf(sheet.getSheetName()) == -1 )
+  {
+    if( selectedCell.getColumn() == COLUMNTOCHECK) 
     {
-      dateTimeCell.setValue('');
-    }
-    else
-    {
-      dateTimeCell.setValue(new Date());
+      var dateTimeCell = selectedCell.offset(DATETIMELOCATION[0],DATETIMELOCATION[1]);
+      if( selectedCell.getDisplayValue() == '')
+      {
+        dateTimeCell.setValue('');
+      }
+      else
+      {
+        dateTimeCell.setValue(new Date());
+      }
     }
   }
 }
+
+  
